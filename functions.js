6a7b47178b065f2b7d13871338b086eac51eb8fc
fouchimi <ousmanefouchimi@gmail.com>
2026-01-10 10:01:10 -0500

    Rest parameters ...numbers

--- functions.js
+++ functions.js
@@ -1,14 +1,32 @@
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
 function add(num1, num2, num3) {
     return num3 ? num1 + num2 + num3 : num1 + num2;
 }
 console.log(add(1, 2));
 console.log(add(1, 2, 3));
-var substract = function (num1, num2, num3) {
+var sub = function (num1, num2, num3) {
     if (num3 === void 0) { num3 = 10; }
     return num1 - num2 - num3;
 };
-console.log(substract(1, 2));
-var mult = function (num1, num2) {
+console.log(sub(1, 2));
+var mul = function (num1, num2) {
     return num1 * num2;
 };
-console.log(mult(5, 6));
+console.log(mul(5, 6));
+function add2(num1, num2) {
+    var num3 = [];
+    for (var _i = 2; _i < arguments.length; _i++) {
+        num3[_i - 2] = arguments[_i];
+    }
+    return num1 + num2 + num3.reduce(function (a, b) { return a + b; }, 0);
+}
+var numbers = [1, 2, 3, 4, 5, 6, 7];
+console.log(add2.apply(void 0, __spreadArray([2, 3], numbers, false)));
--- functions.ts
+++ functions.ts
@@ -6,12 +6,20 @@ console.log(add(1, 2));
 
 console.log(add(1, 2, 3));
 
-const substract = (num1 : number, num2: number, num3 = 10) : number => num1 - num2 - num3;
+const sub = (num1 : number, num2: number, num3 = 10) : number => num1 - num2 - num3;
 
-console.log(substract(1, 2));
+console.log(sub(1, 2));
 
-const mult = function (num1: number, num2: number) : number {
+const mul = function (num1: number, num2: number) : number {
     return num1 * num2;
 }
 
-console.log(mult(5, 6));
+console.log(mul(5, 6));
+
+function add2(num1: number, num2: number, ...num3: number[]): number {
+    return num1 + num2 + num3.reduce((a, b) => a + b, 0);
+}
+
+let numbers = [1, 2, 3, 4, 5, 6, 7];
+
+console.log(add2(2, 3, ...numbers));
