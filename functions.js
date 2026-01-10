41fb919110d327794ee018c7d5e139e0814b57cc
fouchimi <ousmanefouchimi@gmail.com>
2026-01-10 09:54:20 -0500

    Required and optional parameters

--- functions.js
+++ functions.js
@@ -1,4 +1,14 @@
-function add(num1, num2) {
-    return num1 + num2;
+function add(num1, num2, num3) {
+    return num3 ? num1 + num2 + num3 : num1 + num2;
 }
 console.log(add(1, 2));
+console.log(add(1, 2, 3));
+var substract = function (num1, num2, num3) {
+    if (num3 === void 0) { num3 = 10; }
+    return num1 - num2 - num3;
+};
+console.log(substract(1, 2));
+var mult = function (num1, num2) {
+    return num1 * num2;
+};
+console.log(mult(5, 6));
--- functions.ts
+++ functions.ts
@@ -1,5 +1,17 @@
-function add (num1 : number, num2 : number) : number {
-    return num1 + num2;
+function add (num1 : number, num2 : number, num3?: number) : number {
+    return num3 ? num1 + num2 + num3 : num1 + num2;
 }
 
 console.log(add(1, 2));
+
+console.log(add(1, 2, 3));
+
+const substract = (num1 : number, num2: number, num3 = 10) : number => num1 - num2 - num3;
+
+console.log(substract(1, 2));
+
+const mult = function (num1: number, num2: number) : number {
+    return num1 * num2;
+}
+
+console.log(mult(5, 6));
