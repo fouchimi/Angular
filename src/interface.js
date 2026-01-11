78aee66017ff5843e99351bef645ced023afde8a
fouchimi <ousmanefouchimi@gmail.com>
2026-01-10 20:07:30 -0500

    Destructuring array of objects

--- src/interface.js
+++ src/interface.js
@@ -17,3 +17,15 @@ console.log("Employee", employee);
 var _a = { name: "john", id: 1, email: "leboss@gmail.com" }, userName = _a.name, email = _a.email;
 console.log("userName", userName);
 console.log("email", email);
+var _b = [
+    { name: 'John', id: 1, email: 'john@blackstraw.ai' },
+    { name: 'Mike', id: 2, email: 'mike@google.com' },
+    { name: 'Fadil', id: 3, email: 'fadil@lowes.com' },
+    { name: 'Mariam', id: 4, email: 'mariam@nordstrom.com' }
+], user1 = _b[0], user2 = _b[1], restUsers = _b.slice(2);
+console.log(user1, user2);
+console.log(restUsers);
+var filterName = restUsers
+    .filter(function (user) { return user.id > 2; })
+    .map(function (user) { return user.name; });
+console.log("filterName", filterName);
--- src/interface.ts
+++ src/interface.ts
@@ -34,6 +34,22 @@ let { name : userName, email } : User = {name: "john", id: 1, email: "leboss@gma
 console.log("userName", userName);
 console.log("email", email);
 
+let [user1, user2, ...restUsers]: User[] = [
+    {name : 'John', id: 1, email: 'john@blackstraw.ai'}, 
+    {name: 'Mike', id: 2, email: 'mike@google.com'}, 
+    {name: 'Fadil', id: 3, email: 'fadil@lowes.com'},
+    {name: 'Mariam', id: 4, email: 'mariam@nordstrom.com'}
+];
+
+console.log(user1, user2);
+console.log(restUsers);
+
+let filterName = restUsers
+    .filter(user => user.id > 2)
+    .map(user => user.name);
+
+console.log("filterName", filterName);
+
 export interface Login {
     login() : User;
 }
